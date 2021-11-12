840a456b395e21519f702ac5720b4ec274a43ab6
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-11-11 22:18:11 -0600

    Added field validation

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -4,10 +4,22 @@ import { useRouter } from "vue-router";
 import useAuth from "../composable/useAuth";
 import useError from "../composable/useError";
 
-const { isAuthenticated, login, signup, googleLogin} = useAuth();
+import { useForm, useField } from 'vee-validate';
+import * as yup from 'yup';
 
-const username = ref("");
-const password = ref("");
+const schema = yup.object({
+  username: yup.string().required().email(),
+  password: yup.string().required().min(8),
+});
+
+useForm({
+  validationSchema: schema,
+});
+
+const { value: username, errorMessage: emailError } = useField("username");
+const { value: password, errorMessage: passwordError } = useField("password");
+
+const { isAuthenticated, login, signup, googleLogin} = useAuth();
 
 const router = useRouter();
 
@@ -30,7 +42,7 @@ const goToHome = () => {
   if (isAuthenticated.value) {
     router.push("/");
   } else {
-    setError("Invalid username or password");
+    setError("Invalid email or password");
     start();
   }
 };
@@ -51,9 +63,15 @@ const { ready, start } = useTimeout(5000, {controls: true});
 <img class="h-64" src = "../assets/bglogin.png" alt = "Hello BG">
 <form @submit.prevent = "loggingIn" class = "flex flex-col p-4 space-y-4 ">
 
-    <input type = "text" class = "bg-gray-100 border-2 rounded-lg" placeholder = "Username" v-model = "username" />
+    <input
+    name = "username"
+     type = "text" class = "bg-gray-100 border-2 rounded-lg" placeholder = "Email" v-model = "username" />
+     <span class = "text-indigo-700 text-center text-xs">{{emailError}}</span>
 
-    <input type = "password"  class = "bg-gray-100 border-2 rounded-lg" placeholder = "Password" v-model = "password" />
+    <input
+    name = "password"
+     type = "password"  class = "bg-gray-100 border-2 rounded-lg" placeholder = "Password" v-model = "password" />
+     <span class = "text-indigo-700 text-center text-xs">{{passwordError}}</span>
 
     <div class = "flex space-x-2">
     <button type = "submit" @submit.prevent = "loggingIn" class = " w-3/4 py-2 bg-gray-400 font-semibold rounded-lg">Login</button>
