4bffe5eaff771d9e249d452e4db4afce26b6cc0c
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-11-29 21:45:46 -0600

    Chat Layout

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,5 +1,18 @@
 <template>
-  <p>
-    This is the secret page. If you have reached this page then you have successfully logged in and have access to this page. Unauthorized use of this website is prohibited.
-  </p>
+  <h1 class="text-6xl font-extralight tracking-tighter text-center mt-8 bg-gradient-to-r from-purple-400 via-pink-500 to-indigo-500 rounded-xl">The Chat of all Chats!</h1>
+
+  <div class="min-h-[500px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between">
+
+  <ul class= "p-4 space-y-5">
+
+    <li v-for ="n in 5">
+    <div class="flex justify-between bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 px-4 py-2 rounded-xl">
+    <span>Message itself</span><span>By Author Name</span>
+    </div>
+    </li>
+  </ul>
+  <div>
+  <input class ="w-full p-4 rounded-xl focus:outline-none focus:bg-indigo-500" type = "text" placeholder="Type here..." />
+  </div>
+  </div>
 </template>
