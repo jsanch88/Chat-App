bbeafcb6a73c95f87bc7c87b2680f91ec0ffac3f
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-12-03 22:16:30 -0600

    Completed all changes for Chat App

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Vue 3 + Firebase Auth and Google
+
 ## Recommended IDE Setup
+
+# Chat option added on Secret page
 
 - [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,18 +1,55 @@
+<script setup>
+  import { onUnmounted, ref } from 'vue'
+  import useChat from '../composable/useChat'
+  import useAuth from '../composable/useAuth'
+
+  const { messages, unsubscribe, sendMessage } = useChat()
+  const { user } = useAuth()
+
+  const newMessage = ref('')
+
+  const send = () => {
+    sendMessage(newMessage.value)
+    newMessage.value = ''
+  }
+
+  onUnmounted(() => {
+    unsubscribe()
+  })
+</script>
+
 <template>
   <h1 class="text-6xl font-extralight tracking-tighter text-center mt-8 bg-gradient-to-r from-purple-400 via-pink-500 to-indigo-500 rounded-xl">The Chat of all Chats!</h1>
-
-  <div class="min-h-[500px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between">
-
-  <ul class= "p-4 space-y-5">
-
-    <li v-for ="n in 5">
-    <div class="flex justify-between bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 px-4 py-2 rounded-xl">
-    <span>Message itself</span><span>By Author Name</span>
+  <div
+    class="
+      min-h-[500px]
+      w-full
+      mt-8
+      rounded-lg
+      shadow-2xl
+      flex flex-col
+      justify-between
+    "
+  >
+    <ul class="p-4 space-y-4">
+      <li v-for="message in messages" :key="message.id">
+        <div
+          class="flex justify-between px-4 py-2 rounded-lg"
+          :class="user === message.author ? 'bg-gradient-to-r from-purple-400 via-pink-500 to-indigo-500' : 'bg-gradient-to-r from-indigo-300 via-pink-200 to-red-500'"
+        >
+          <span>{{ message.text }}</span
+          ><span>by {{ message.author }}</span>
+        </div>
+      </li>
+    </ul>
+    <div>
+      <input
+        class="w-full p-4 rounded-lg focus:outline-none focus:bg-gradient-to-r from-pink-800 via-pink-500 to-indigo-500"
+        type="text"
+        placeholder="Type here and press enter to send..."
+        v-model="newMessage"
+        @change="send"
+      />
     </div>
-    </li>
-  </ul>
-  <div>
-  <input class ="w-full p-4 rounded-xl focus:outline-none focus:bg-indigo-500" type = "text" placeholder="Type here..." />
-  </div>
   </div>
 </template>
