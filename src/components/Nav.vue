a572501e4549da59d4d97626eca2d8024fcb2dad
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-12-03 22:33:16 -0600

    Changes made to Nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,31 +13,31 @@ const loggingOut = () => {
 </script>
 
 <template>
-<div class="bg-red-500 text-black">
+<div class="bg-gradient-to-r from-pink-800 via-pink-500 to-indigo-500 text-white">
 <div class = "container mx-auto flex items-center justify-between">
 
 <h1 class="tracking-tighter text-3xl font-bold">
-Vue<span class="bg bg-indigo-600 font-normal">RealAuth with Google</span>
+Vue<span class="bg-indigo-600 font-normal">RealAuth with Google and Chat :D</span>
 </h1>
     
     <nav>
         <ul class= "flex space-x-4">
         <router-link to = "/">
-            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Home</li></router-link>
+            <li class="bg-pink-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Home</li></router-link>
 
             <router-link  to = "/about">
-            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">About</li></router-link>
+            <li class="bg-pink-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">About</li></router-link>
 
             <router-link v-if = "!isAuthenticated" to = "/login">
-            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Login</li></router-link>
+            <li class="bg-pink-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Login</li></router-link>
 
             <div v-else class = "flex">
 
             <router-link to = "/secret">
-            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Secret</li></router-link>
+            <li class="bg-pink-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Secret</li></router-link>
 
             <button  @click = "loggingOut">
-            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Logout</li></button>
+            <li class="bg-pink-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Logout</li></button>
 
             </div>
 
